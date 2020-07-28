1cd721e57c40f601065ab37e8211853bd2dca851
Richard <evildea@gmail.com>
2020-07-28 23:25:20 +1000

    Mi aldonis novan sekcion

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,6 +51,7 @@ module.exports = {
           '/proposals/draft/communityAdministration',
           '/proposals/draft/countryNames',
           '/proposals/draft/logo',
+          '/proposals/draft/meteorology',
           '/proposals/draft/name',
           '/proposals/draft/numberBase',
           '/proposals/draft/numbers',
